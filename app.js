02e041a3bf32803165e638849d20b8ba969449c6
Philippe Ngom <philngom@gmail.com>
2021-11-04 14:23:27 -0700

    header now updates to display most recently clicked image

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const horseAudio = document.querySelector('.horse-audio');
 const catImage = document.querySelector('.cat-image');
 const dogImage = document.querySelector('.dog-image');
 const horseImage = document.querySelector('.horse-image');
+const lastAnimalPlayed = document.getElementById('recent-animal-played');
 
 // initialize global state
 
@@ -13,16 +14,19 @@ const horseImage = document.querySelector('.horse-image');
   // use user input to update state
   // update DOM to reflect the new state
 catImage.addEventListener('click', () => {
+    lastAnimalPlayed.textContent = 'You clicked the cat image';
     catAudio.currentTime = 0;
     catAudio.play();
 });
 
 dogImage.addEventListener('click', () => {
+    lastAnimalPlayed.textContent = 'You clicked the dog image';
     dogAudio.currentTime = 0;
     dogAudio.play();
 });
 
 horseImage.addEventListener('click', () => {
+    lastAnimalPlayed.textContent = 'You clicked the horse image';
     horseAudio.currentTime = 0;
     horseAudio.play();
 });
